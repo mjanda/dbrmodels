487c3385c4e58d022a578f436b8f2952a06df9ca
Vitalii Ishenko <finalist736@gmail.com>
2016-02-29 18:58:20 +0300

    directory structure changes, bug fixing

--- README.md
+++ README.md
@@ -1,29 +1,60 @@
 # dbrmodels
-Create projects for MySql database and generate table model structs for github.com/gocraft/dbr
+Create projects for MySql database and generate table models structs for github.com/gocraft/dbr
 ## Getting started
 Lets create MySql table
 ```sql
 CREATE TABLE Persons
 (
-    PersonID    int,
+    PersonID    int(11) unsigned NOT NULL AUTO_INCREMENT,
     LastName    varchar(255),
     FirstName   varchar(255),
-    Address     varchar(255),
-    City        varchar(255)
+    Address     varchar(255) DEFAULT NULL,
+    City        varchar(255) DEFAULT NULL,
+	BirthDay 	date NOT NULL,
+	PRIMARY KEY (`PersonID`)
 );
 ```
 and generate gocraft/dbr model
 ```go
-package dbmodels
+package Persons
 
 import "github.com/gocraft/dbr"
 
+var fieldsNames = []string{"PersonID", "LastName", "FirstName", "Address", "City"}
+var auto_increment_filed string = "PersonID"
+
 type Persons struct {
-    PersonID    int64           `db:"PersonID"`
-    LastName    string          `db:"LastName"`
-    FirstName   string          `db:"FirstName"`
-    Address     dbr.NullString  `db:"Address"`
-    City        dbr.NullString  `db:"City"`
+	PersonID  int64          `db:"PersonID"`
+	LastName  string         `db:"LastName"`
+	FirstName string         `db:"FirstName"`
+	Address   dbr.NullString `db:"Address"`
+	City      dbr.NullString `db:"City"`
+	BirthDay  dbr.NullTime   `db:"BirthDay"`
+}
+
+func New() *Persons {
+	return new(Persons)
+}
+
+func NewSlice() []*Persons {
+	return make([]*Persons, 0)
+}
+
+// return fields names
+func FieldsNames() []string {
+	return fieldsNames
+}
+
+// return fields names with out auto_increment field
+func FieldsNamesWithOutAI() []string {
+	var slice []string
+	for _, iterator := range fieldsNames {
+		if iterator == auto_increment_filed {
+			continue
+		}
+		slice = append(slice, iterator)
+	}
+	return slice
 }
 ```
 ## MySql types
@@ -32,6 +63,7 @@ type Persons struct {
 | tinyint(1) | bool | dbr.NullBool |
 | int | int64 | dbr.NullInt64 |
 | float | float64 | dbr.NullFloat64 |
+| date, datetime, timestamp | dbr.NullTime | dbr.NullTime |
 | * | string | dbr.NullString |
 
 # install
@@ -41,19 +73,55 @@ go get github.com/finalist736/dbrmodels
 ## gocraft/dbr example
 ```go
 // Get a record
-var persons dbmodels.Persons
-err := dbrSess.Select("*").From("persons").Where("PersonID = ?", 13).LoadStruct(&persons)
+var persons Persons.Persons
+err := dbrSess.Select("*").From("Persons").Where("PersonID = ?", 1).Load(&persons)
+
+// insert record
+result, err := dbrSess.InsertInto("Persons").Columns(Persons.FieldsNamesWithOutAI()...).Record(&persons).Exec()
+newAIID := result.LastInsertID()
+
+// update record
+_, err = dbrSess.Update("Persons").Set("Address", "far far away").Where("`PersonID`=?", newAIID).Exec()
 ```
 # Projects
 
-Project including next data:
-* Name
+dbrmodels works with projects(databases) including next data:
+* Project name
 * DB Host
 * DB Port
 * DB User
 * DB Password
 * DB Name
 * Path where to .go files located
+
+### Example 
+```json
+{
+        "Name": "test",
+        "DBHost": "localhost",
+        "DBPort": "3306",
+        "DBUser": "root",
+        "DBPass": "",
+        "DBName": "test",
+        "Path": "/home/dbrfan/go/src/github.com/dbrfan/dbrmodels"
+}
+```
+
+# Directory structure
+
+```sql
+SHOW TABLES;
+- Persons
+- TestDatesTable
+```
+```
+- /home/dbrfan/go/src/github.com/dbrfan
+---- dbrmodels
+--------- Persons
+-------------- models.go
+--------- TestDatesTable
+-------------- models.go
+```
 
 # Using
 start generate project
--- table.go
+++ table.go
@@ -9,35 +9,11 @@ import (
 	"text/template"
 )
 
-type Field struct {
-	Name string
-	Type string
-	Tag  string
-}
-
-type TplData struct {
-	PkgName     string
-	Table       string
-	Fields      []Field
-	FieldsNames string
-	DbrUsed     string
-}
-
-func CreateTableModel(path string, table string, db *sql.DB, verbose bool) {
-	var (
-		name  string
-		typ   string
-		null  string
-		key   string
-		def   string
-		extra string
-	)
-
-	const data = `package {{.PkgName}}
-
-import {{.DbrUsed}}"github.com/gocraft/dbr"
+const data = `package {{.PkgName}}
+{{if .DbrUsed}}import "github.com/gocraft/dbr"{{end}}
 
 var fieldsNames = []string{ {{.FieldsNames}} }
+{{if .AutoInc}}var auto_increment_field string = "{{.AutoInc}}"{{end}}
 
 type {{.Table}} struct {
 	{{range .Fields}}{{.Name}}{{/*tab*/}} {{.Type}}{{/*tab*/}} {{.Tag}}
@@ -55,27 +31,50 @@ func NewSlice() []*{{.Table}} {
 func FieldsNames() []string {
 	return fieldsNames
 }
-
-func FieldsNamesWithOutID() []string {
-	slice := make([]string, 0)
+{{if .AutoInc}}
+func FieldsNamesWithOutAI() []string {
+	var slice []string
 	for _, iterator := range fieldsNames {
-		if iterator == "ID" {
+		if iterator == auto_increment_field {
 			continue
 		}
 		slice = append(slice, iterator)
 	}
 	return slice
 }
-
+{{end}}
 `
 
-	d := TplData{}
-	d.PkgName = table
-	d.Table = strings.Title(table)
-	d.DbrUsed = "_"
+type Field struct {
+	Name string
+	Type string
+	Tag  string
+}
 
-	var dbrUsed int
+type TplData struct {
+	PkgName     string
+	Table       string
+	Fields      []Field
+	FieldsNames string
+	AutoInc     string
+	DbrUsed     bool
+}
 
+func CreateTableModel(path, table string, db *sql.DB, verbose bool) {
+	var (
+		name  string
+		typ   string
+		null  string
+		key   string
+		def   sql.NullString
+		extra string
+	)
+
+	template_data := TplData{}
+	template_data.PkgName = table
+	template_data.Table = strings.Title(table)
+
+	// get table columns info
 	q := fmt.Sprintf("SHOW COLUMNS FROM %s", table)
 	if rows, err := db.Query(q); err == nil {
 		if verbose {
@@ -84,35 +83,44 @@ func FieldsNamesWithOutID() []string {
 		for rows.Next() {
 			err := rows.Scan(&name, &typ, &null, &key, &def, &extra)
 			if err != nil {
-				fmt.Errorf("%s", err.Error())
+				fmt.Fprintf(os.Stderr, "rows.Scan: %s\n", err.Error())
+				continue
 			}
 			if verbose {
-				fmt.Printf("\t\t`%s` %s\n", name, typ)
+				fmt.Printf("\t\tname: `%s` type: %s null: %s key: %s def: %s extra: %s\n", name, typ, null, key, def.String, extra)
 			}
-			if typ == "tinyint(1)" {
+			titled_name := strings.Title(name)
+			if extra == "auto_increment" {
+				template_data.AutoInc = titled_name
+			}
+
+			if typ == "tinyint(1)" { // bool need be first because next `strings.Contains(typ, "int")`
 				if null == "YES" {
-					dbrUsed++
+					template_data.DbrUsed = true
 					typ = "dbr.NullBool"
 				} else {
 					typ = "bool"
 				}
 			} else if strings.Contains(typ, "int") {
 				if null == "YES" {
-					dbrUsed++
+					template_data.DbrUsed = true
 					typ = "dbr.NullInt64"
 				} else {
 					typ = "int64"
 				}
 			} else if strings.Contains(typ, "float") {
 				if null == "YES" {
-					dbrUsed++
+					template_data.DbrUsed = true
 					typ = "dbr.NullFloat64"
 				} else {
 					typ = "float64"
 				}
+			} else if strings.Contains(typ, "date") || strings.Contains(typ, "timestamp") {
+				template_data.DbrUsed = true
+				typ = "dbr.NullTime"
 			} else {
 				if null == "YES" {
-					dbrUsed++
+					template_data.DbrUsed = true
 					typ = "dbr.NullString"
 				} else {
 					typ = "string"
@@ -121,28 +129,27 @@ func FieldsNamesWithOutID() []string {
 
 			tag := fmt.Sprintf("`db:\"%s\"`", name)
 			if verbose {
-				fmt.Printf("\t\t\t => %s %s %s\n", name, typ, tag)
+				fmt.Printf("\t\t\t => %s %s %s\n", titled_name, typ, tag)
 			}
-			f := Field{strings.Title(name), typ, tag}
-			d.Fields = append(d.Fields, f)
-			d.FieldsNames = fmt.Sprintf("%s, \"%s\"", d.FieldsNames, f.Name)
+			table_field := Field{titled_name, typ, tag}
+			template_data.Fields = append(template_data.Fields, table_field)
+			template_data.FieldsNames = fmt.Sprintf("%s, \"%s\"", template_data.FieldsNames, table_field.Name)
 		}
-		d.FieldsNames = strings.Trim(d.FieldsNames, ",")
-	}
-	if dbrUsed > 0 {
-		d.DbrUsed = ""
+		template_data.FieldsNames = strings.Trim(template_data.FieldsNames, ",")
 	}
 	t := template.Must(template.New("struct").Parse(data))
+
 	fullPath := path + "/" + table
 	fullFileName := fullPath + "/model.go"
 	err := os.MkdirAll(fullPath, 0700)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
+
 	file, err := os.Create(fullFileName)
 	defer file.Close()
 	if err == nil {
-		if err := t.Execute(file, d); err != nil {
+		if err := t.Execute(file, template_data); err != nil {
 			fmt.Errorf("%s", err.Error())
 		}
 		cmd := exec.Command("go", "fmt", fullFileName)
